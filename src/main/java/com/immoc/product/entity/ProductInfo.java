adff9a5fe10206e63edcfbe717065862553d818c
bryan <bryan.zhu@gmail.com>
2018-10-16 00:09:22 +0800

    创建ProductInfo实体类，添加lombok依赖(在IDEA中安装lombok插件)

---
+++ src/main/java/com/immoc/product/entity/ProductInfo.java
@@ -0,0 +1,50 @@
+package com.immoc.product.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+/**
+ * @author Bryan Zhu
+ * @date 2018-10-15 23:55
+ */
+@Data
+@Entity
+public class ProductInfo {
+
+    /** 商品ID */
+    @Id
+    private int productId;
+
+    /** 商品名 */
+    private String productName;
+
+    /** 商品价格 */
+    private BigDecimal productPrice;
+
+    /** 商品库存 */
+    private int productStock;
+
+    /** 商品描述 */
+    private String productDescription;
+
+    /** 商品图标 */
+    private String productIcon;
+
+    /** 商品状态 0 正常， 1 下架*/
+    private int productStatus;
+
+    /** 类目编号 */
+    private int categoryType;
+
+    /** 创建日期 */
+    private Date createTime;
+
+    /** 修改日期 */
+    private Date updateTime;
+
+
+}
