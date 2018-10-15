7249dcab0cb0151746d5c0c331a194858b783d22
bryan <bryan.zhu@gmail.com>
2018-10-16 00:34:21 +0800

    添加根据产品状态查询产品的方法，并创建单元测试

--- src/main/java/com/immoc/product/repository/ProductInfoRepository.java
+++ src/main/java/com/immoc/product/repository/ProductInfoRepository.java
@@ -3,6 +3,8 @@ package com.immoc.product.repository;
 import com.immoc.product.entity.ProductInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author Bryan Zhu
  * @date 2018-10-16 0:11
@@ -10,4 +12,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductInfoRepository extends JpaRepository<ProductInfo, String> {
 
 
+    /** 查询所有在架商品 */
+    List<ProductInfo> findByProductStatus(Integer productStatus);
 }
